36cf1cf6f8283e94b15d97761371b8b86bb3e750
Jai <rayeejaya@gmail.com>
2017-11-14 22:33:47 -0500

    Initial set up react Redux proect

---
+++ server/index.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import path from 'path';
+
+let app = express();
+app.get('/*', (req, res)=> {
+    res.sendfile(path.join(__dirname, './index.html'));
+});
+app.listen(3000, () => console.log('Running localhost:3000'));
